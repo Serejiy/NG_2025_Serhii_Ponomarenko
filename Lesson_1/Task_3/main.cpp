b1147a3d9f54d6588eda9bf3e415371dd946f8bf
Serejiy <nomi45148@gmail.com>
2025-04-20 18:20:48 +0200

    Task_3 started, romboid completed

---
+++ Lesson_1/Task_3/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int amount;
+    cout << "Enter the amount of stars: ";
+    cin >> amount;
+
+    if (amount % 2 == 0) {
+        amount += 1;
+    }
+
+    int mid = amount / 2;
+    int left = mid;
+    int right = mid;
+
+    for (int row = 0; row < amount; row++) {
+        for (int col = 0; col < amount; col++) {
+            if (col >= left && col <= right) {
+                cout << "*";
+            } else {
+                cout << "1";
+            }
+        }
+
+
+        if (row < mid) {
+            left--;
+            right++;
+        } else {
+            left++;
+            right--;
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
