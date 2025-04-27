7391d60025db62eced6acf8ac87c83de923469b7
Ponomarenko Sergiy <110176064+Serejiy@users.noreply.github.com>
2025-04-27 12:03:19 +0200

    comments deleted

--- Lesson_1/Task_3/main.cpp
+++ Lesson_1/Task_3/main.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
-#include <cmath>  // Для функции abs
+#include <cmath>
 using namespace std;
 
-// Функция для рисования ромба
 void romboid(int amount) {
     if (amount % 2 == 0) {
-        amount += 1; // Делаем нечётным для красивого ромба
+        amount += 1;
     }
 
     int mid = amount / 2;
@@ -33,7 +32,7 @@ void romboid(int amount) {
     }
 }
 
-// Функция для рисования спирали
+
 void spiral(int size) {
     for (int row = 0; row < size; ++row) {
         for (int col = 0; col < size; ++col) {
@@ -50,10 +49,10 @@ void spiral(int size) {
             int spiral_layers = size / 2;
             if (size % 4 == 0) spiral_layers--;
 
-            // Исправить диагональ сверху слева направо вниз
+            
             if (row == col + 1 && row <= spiral_layers) distance_from_center++;
 
-            // Печать звезды или пробела
+            
             cout << ((distance_from_center + size / 2) % 2 == 0 ? "*" : " ");
         }
         cout << endl;
